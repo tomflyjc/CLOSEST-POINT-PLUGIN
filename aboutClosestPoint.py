da96a17ab9751cc341731e6372c0df650d037421
tomflyjc <jeanchristophebaudin@ymail.com>
2019-02-05 18:23:40 +0100

    Update and rename about.py to aboutClosestPoint.py

--- aboutClosestPoint.py
+++ aboutClosestPoint.py
@@ -1,69 +1,79 @@
 # -*- coding: utf-8 -*-
 
+
+import os
 import os.path
-from PyQt4 import QtCore, QtGui
+from qgis.core import QgsProject, QgsMapLayer, QgsWkbTypes
+from qgis.PyQt.QtCore import QFileInfo, QSettings, QCoreApplication
+from qgis.PyQt.QtCore import QTranslator, qVersion
+from qgis.PyQt.QtGui import QIcon,QFont,QPalette,QBrush,QColor
+from PyQt5 import QtCore
+from qgis.PyQt.QtWidgets import QAction, QMessageBox,QDialog, QDialogButtonBox,QAction,QGridLayout,QLabel,QTextEdit,QPushButton,QFrame,QSpacerItem,QSizePolicy,QApplication
+# Import libs 
+
+import sys
 
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
         Dialog.setObjectName("Dialog")
         Dialog.resize(QtCore.QSize(QtCore.QRect(0,0,440,660).size()).expandedTo(Dialog.minimumSizeHint()))
 
-        self.gridlayout = QtGui.QGridLayout(Dialog)
+        self.gridlayout = QGridLayout(Dialog)
         self.gridlayout.setObjectName("gridlayout")
 
-        font = QtGui.QFont()
+        font = QFont()
         font.setPointSize(15) 
         font.setWeight(50) 
         font.setBold(True)
         
-        self.label_2 = QtGui.QLabel(Dialog)
+        self.label_2 = QLabel(Dialog)
         self.label_2.setFont(font)
         self.label_2.setTextFormat(QtCore.Qt.RichText)
         self.label_2.setObjectName("label_2")
         self.gridlayout.addWidget(self.label_2,1,1,1,2)
          
-        self.textEdit = QtGui.QTextEdit(Dialog)
+        self.textEdit = QTextEdit(Dialog)
 
-        palette = QtGui.QPalette()
+        palette = QPalette()
 
-        brush = QtGui.QBrush(QtGui.QColor(0,0,0,0))
+        brush = QBrush(QColor(0,0,0,0))
         brush.setStyle(QtCore.Qt.SolidPattern)
-        palette.setBrush(QtGui.QPalette.Active,QtGui.QPalette.Base,brush)
+        palette.setBrush(QPalette.Active,QPalette.Base,brush)
 
-        brush = QtGui.QBrush(QtGui.QColor(0,0,0,0))
+        brush = QBrush(QColor(0,0,0,0))
         brush.setStyle(QtCore.Qt.SolidPattern)
-        palette.setBrush(QtGui.QPalette.Inactive,QtGui.QPalette.Base,brush)
+        palette.setBrush(QPalette.Inactive,QPalette.Base,brush)
 
-        brush = QtGui.QBrush(QtGui.QColor(255,255,255))
+        brush = QBrush(QColor(255,255,255))
         brush.setStyle(QtCore.Qt.SolidPattern)
-        palette.setBrush(QtGui.QPalette.Disabled,QtGui.QPalette.Base,brush)
+        palette.setBrush(QPalette.Disabled,QPalette.Base,brush)
         self.textEdit.setPalette(palette)
         self.textEdit.setAutoFillBackground(True)
         self.textEdit.width = 320
         self.textEdit.height = 360
-        self.textEdit.setFrameShape(QtGui.QFrame.NoFrame)
-        self.textEdit.setFrameShadow(QtGui.QFrame.Plain)
+        self.textEdit.setFrameShape(QFrame.NoFrame)
+        self.textEdit.setFrameShadow(QFrame.Plain)
         self.textEdit.setReadOnly(True)
         self.textEdit.setObjectName("textEdit")
         self.textEdit.setTextInteractionFlags(QtCore.Qt.TextBrowserInteraction)
        
         self.gridlayout.addWidget(self.textEdit,2,1,5,2) 
 
-        self.pushButton = QtGui.QPushButton(Dialog)
+        self.pushButton = QPushButton(Dialog)
         self.pushButton.setObjectName("pushButton")
         self.gridlayout.addWidget(self.pushButton,4,2,1,1) 
 
-        spacerItem = QtGui.QSpacerItem(20,40,QtGui.QSizePolicy.Minimum,QtGui.QSizePolicy.Expanding)
+        spacerItem = QSpacerItem(20,40,QSizePolicy.Minimum,QSizePolicy.Expanding)
         self.gridlayout.addItem(spacerItem,3,1,1,1)
 
         self.retranslateUi(Dialog)
-        QtCore.QObject.connect(self.pushButton,QtCore.SIGNAL("clicked()"),Dialog.reject)
+        self.pushButton.clicked.connect(Dialog.reject)
         QtCore.QMetaObject.connectSlotsByName(Dialog)
 
     def retranslateUi(self, Dialog):
-        Dialog.setWindowTitle(QtGui.QApplication.translate("Dialog", "ClosestPoint", None, QtGui.QApplication.UnicodeUTF8))
-        self.label_2.setText(QtGui.QApplication.translate("Dialog", "ClosestPoint 0.1", None, QtGui.QApplication.UnicodeUTF8))
-        self.textEdit.setHtml(QtGui.QApplication.translate("Dialog", "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+        Dialog.setWindowTitle(QApplication.translate("Dialog", "ClosestPoint", None))
+        self.label_2.setText(QApplication.translate("Dialog", "ClosestPoint 0.1", None))
+        self.textEdit.setHtml(QApplication.translate("Dialog", "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
         "p, li { white-space: pre-wrap; }\n"
         "</style></head><body style=\" font-family:\'Sans Serif\'; font-size:8pt; font-weight:400; font-style:normal;\">\n"
         "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:\'MS Shell Dlg 2\'; font-size:8pt;\"><span style=\" font-weight:600;\">"
@@ -97,8 +107,8 @@ class Ui_Dialog(object):
         "<p style=\"margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">"
         "                   "
         "<br><b>jeanchristophebaudin@ymail.com</b><br>"
-        "<br><br><i>code 0.1 (04 september 2017).</i></p></body></html>", None, QtGui.QApplication.UnicodeUTF8))
-        self.pushButton.setText(QtGui.QApplication.translate("Dialog", "OK", None, QtGui.QApplication.UnicodeUTF8))
+        "<br><br><i>code 0.1 (05 february 2019).</i></p></body></html>", None))
+        self.pushButton.setText(QApplication.translate("Dialog", "OK", None))
 
 
 
